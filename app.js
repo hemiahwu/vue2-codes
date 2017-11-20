1be582162258b0f3787d1e6129c6074a186f1ab3
HenryWu <27732357@qq.com>
2017-11-20 11:21:46 +0800

    add

---
+++ app.js
@@ -0,0 +1,17 @@
+new Vue({
+    el: '#vue-app',
+    data: {
+        characters: ['Mario', 'Luigi', 'Yoshi', 'Bowser'],
+        ninjas: [
+            { name: 'Ryu', age: 25 },
+            { name: 'Yoshi', age: 35 },
+            { name: 'Ken', age: 55 }
+        ]
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+});
