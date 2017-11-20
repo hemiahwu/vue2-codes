774cde2e987d12339dbb9c14c06d8414fb986549
HenryWu <27732357@qq.com>
2017-11-20 11:36:18 +0800

    add

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <app-header></app-header>
+        <app-ninjas v-bind:ninjas="ninjas"></app-ninjas>
+        <app-footer></app-footer>
+    </div>
+</template>
+
+<script>
+// Imports
+import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
+import Ninjas from './components/Ninjas.vue';
+
+export default {
+    components: {
+        'app-header': Header,
+        'app-footer': Footer,
+        'app-ninjas': Ninjas
+    },
+    data () {
+        return {
+          ninjas: [
+              {name: 'Ryu', speciality: 'Vue Components', show: false},
+              {name: 'Crystal', speciality: 'HTML Wizardry', show: false},
+              {name: 'Hitoshi', speciality: 'Click Events', show: false},
+              {name: 'Tango', speciality: 'Conditionals', show: false},
+              {name: 'Kami', speciality: 'Webpack', show: false},
+              {name: 'Yoshi', speciality: 'Data Diggin', show: false}
+          ]
+        }
+    }
+}
+</script>
+
+<style>
+body{
+    margin: 0;
+    font-family: 'Nunito SemiBold';
+}
+</style>
