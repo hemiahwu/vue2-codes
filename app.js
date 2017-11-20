eca269e7e871d4ba59659dba8fc40ee2d705fc35
HenryWu <27732357@qq.com>
2017-11-20 11:13:49 +0800

    add

---
+++ app.js
@@ -0,0 +1,14 @@
+new Vue({
+    el: '#vue-app',
+    data: {
+
+    },
+    methods: {
+        logName: function(){
+            console.log('you entered your name');
+        },
+        logAge: function(){
+            console.log('you entered your age');
+        }
+    }
+});
