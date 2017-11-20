12fed7c81ffd372604b33bee5fcbbe5a16accd64
HenryWu <27732357@qq.com>
2017-11-20 11:40:26 +0800

    add

---
+++ src/components/Footer.vue
@@ -0,0 +1,30 @@
+<template>
+    <footer>
+        <p>Copyright 2017 {{ title }}</p>
+    </footer>
+</template>
+<script>
+export default {
+    props: {
+      title: {
+        type: String,
+        required: true
+      }
+    },
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
+<style scoped>
+footer{
+    background: #222;
+    padding: 6px;
+}
+p{
+        color: lightgreen;
+        text-align: center;
+}
+</style>
