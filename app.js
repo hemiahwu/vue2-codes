cf97937ee8443069a0d57f19f70d88aece4df11c
HenryWu <27732357@qq.com>
2017-11-20 11:02:12 +0800

    add

---
+++ app.js
@@ -0,0 +1,20 @@
+new Vue({
+    el: '#vue-app',
+    data: {
+        age: 25,
+        x: 0,
+        y: 0
+    },
+    methods: {
+        add: function(inc){
+            this.age += inc;
+        },
+        subtract: function(dec){
+            this.age -= dec;
+        },
+        updateXY: function(event){
+            this.x = event.offsetX;
+            this.y = event.offsetY;
+        }
+    }
+});
