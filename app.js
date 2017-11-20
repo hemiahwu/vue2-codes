ccd0e0ece5e51cfe8c894f0f3dbb21d721f8d325
HenryWu <27732357@qq.com>
2017-11-20 11:26:58 +0800

    add

---
+++ app.js
@@ -0,0 +1,16 @@
+Vue.component('greeting', {
+    template: '<p>组件应用</p>'
+});
+
+/* new Vue({
+    el: '.test',
+    template: '<p>I am a template</p>'
+}); */
+
+new Vue({
+    el: '#vue-app-one'
+});
+
+new Vue({
+    el: '#vue-app-two'
+});
