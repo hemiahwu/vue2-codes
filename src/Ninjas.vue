350f378e1aa7b869512972b25763f9f50958cbb3
HenryWu <27732357@qq.com>
2017-11-20 11:33:55 +0800

    add

---
+++ src/Ninjas.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h1>List of ninjas</h1>
+        <ul>
+            <li v-for="ninja in ninjas">{{ ninja }}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+          ninjas: ['Yoshi', 'Mario', 'Ryu']
+        }
+    }
+}
+</script>
+
+<style scoped>
+h1{
+    color: green;
+}
+li{
+    list-style-type: none;
+    padding: 6px;
+}
+</style>
